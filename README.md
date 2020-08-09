0402e0267308118f2303e416d305adad6c29906c
RetownPlato <allenplato28@gmail.com>
2020-08-09 21:52:27 +0800

    OK 1&2

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-Install GNU ARM toolchain
+The code is dedicated for rapsberry pi 3B.
+
+1. Install GNU ARM toolchain
+
 For Mac OS X, you can use the [YAGARTO](http://www.yagarto.org/) packages.
 For Linux, see [instruction](install.md)
+
+2. `make kernel.img`
 ```
 cd template
 make
 ```
 If any errors occur, please refer to the troubleshooting section. 
+
 If not, you will have generated three files. `kernel.img` is the compiled image of your operating system. `kernel.list` is a listing of the assembly code you wrote, as it was actually generated. This is useful to check that things were generated correctly in future. The `kernel.map` file contains a map of where all the labels ended up, which can be useful for chasing around values.
+
 To install your operating system, first of all get a Raspberry PI SD card which has an operating system installed already. If you browse the files in the SD card, you should see one called `kernel.img`. Rename this file to something else, such as `kernel_linux.img`. Then, copy the file `kernel.img` that make generated onto the SD Card. You've just replaced the existing operating system with your own. To switch back, simply delete your `kernel.img` file, and rename the other one back to `kernel.img`. I find it is always helpful to keep a backup of you original Raspberry Pi operating system, in case you need it again.
+
 Put the SD card into a Raspberry Pi and turn it on.
--- template/source/main.s
+++ template/source/main.s
@@ -1,7 +1,7 @@
 .section .init      @ where to put our code
 .globl _start       @ 
 _start:
-@ 0x20200000 is the address of GPIO Function Select 0 register
+@ 0x20200000 is the base address of GPIO Function Select registers.
 ldr r0,=0x20200000  @ ldr reg,=val puts the number val into the register named reg
 @ r1=0x00020000, GPIO Function Select 1 register=r1
 @ Enable output to the 16th PGIO pin
@@ -14,6 +14,17 @@ str r1,[r0,#4]      @ str reg,[dest,#val] stores the number in reg at the addres
 mov r1,#1           @ r1=1
 lsl r1,#16          
 str r1,[r0,#40]     @ GPIO Pin Output Clear 00x 7E20 0028
-@ The new line at the end of the block is intentional.
-loop$:              @ name: labels the next line name.
-b loop$             @ b label causes the next line to be executed to be label.
+
+@ waiting
+mov r2,#0x3F0000
+wait1$:
+sub r2,#1           @ sub reg,#val subtracts the number val from the value in reg
+cmp r2,#0           @ cmp reg,#val compares the value in reg with the number val.
+bne wait1$          @ bne means the command to be executed once if the last comparison 
+                    @ determined that the numbers were not equal
+str r1,[r0,#28]     @ Set GPIO 16 to high, causing the LED to turn off.
+mov r2,#0x3F0000
+b wait1$
+@ @ The new line at the end of the block is intentional.
+@ loop$:              @ name: labels the next line name.
+@ b loop$             @ b label causes the next line to be executed to be label.
